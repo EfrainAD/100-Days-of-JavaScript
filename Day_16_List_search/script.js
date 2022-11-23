311209cf3ba633bb885e4a4b037a3a29cc18ee6e
EfrainAD <105945226+EfrainAD@users.noreply.github.com>
2022-11-22 21:32:41 -0500

    Day 16 - Made updated by looking at the solution, but did it with a .forEach instead

--- Day_16_List_search/script.js
+++ Day_16_List_search/script.js
@@ -1,19 +1,12 @@
-const ul = document.querySelector('#list')
 const list = document.querySelectorAll('li')
 const searchBar = document.querySelector('#search')
-const defaultItems = []
-list.forEach(item => defaultItems.push(item.innerHTML))
-console.log(defaultItems)
 
 searchBar.addEventListener('input', (e) => {
      searchValue = e.target.value
-     
-     newList = defaultItems.filter(item => item.toUpperCase().includes(searchValue.toUpperCase()))
-     ul.innerHTML = ''
-     
-     newList.forEach(item => {
-          const li = document.createElement('li')
-          li.innerHTML = item
-          ul.append(li)
+     list.forEach((item) => {
+          if (item.innerHTML.toUpperCase().includes(searchValue.toUpperCase()))
+               item.style.display = 'block'
+          else 
+               item.style.display = 'none'
      })
 })
