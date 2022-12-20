c74ff868ce73daf6ce3350639fa411a82261f554
EfrainAD <105945226+EfrainAD@users.noreply.github.com>
2022-12-20 17:54:02 -0500

    Day 22 changed based on slution, things I look for in hopes to get better.

--- Day_22_Active_Menu/script.js
+++ Day_22_Active_Menu/script.js
@@ -1,8 +1,10 @@
 const links = document.querySelectorAll('.link')
 
 const changeActiveLink =(e) => {
-     links.forEach(link => link.classList.remove('active'))
+     // links.forEach(link => link.classList.remove('active'))
+     document.querySelector('.link.active').classList.remove('active')
      e.target.parentNode.classList.add('active')
 }
 
-links.forEach(link => link.addEventListener('click', changeActiveLink)) 
+// links.forEach(link => link.addEventListener('click', changeActiveLink)) 
+document.querySelector('.nav-list').addEventListener('click', changeActiveLink)
