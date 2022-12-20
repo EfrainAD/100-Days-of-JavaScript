8ef69ecb2f0d4e85d83e85c0922773fe19369cc9
EfrainAD <105945226+EfrainAD@users.noreply.github.com>
2022-12-20 16:13:21 -0500

    Day 22 setup

---
+++ Day_22_Active_Menu/script.js
@@ -0,0 +1,8 @@
+const links = document.querySelectorAll('.link')
+
+const changeActiveLink =(e) => {
+     links.forEach(link => link.classList.remove('active'))
+     e.target.parentNode.classList.add('active')
+}
+
+links.forEach(link => link.addEventListener('click', changeActiveLink))
