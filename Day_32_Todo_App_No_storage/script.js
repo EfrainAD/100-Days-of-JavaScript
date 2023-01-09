1ae9edc13594b57625a7e6bf182c338f728d38cd
EfrainAD <105945226+EfrainAD@users.noreply.github.com>
2023-01-09 13:59:26 -0500

    Added check if user sure

--- Day_32_Todo_App_No_storage/script.js
+++ Day_32_Todo_App_No_storage/script.js
@@ -3,20 +3,29 @@ const submit = document.querySelector('form button')
 const todoList = document.querySelector('.todo-list')
 const clear = document.querySelector('.clear')
 
+const deleteTask = (task) => {
+     const reply = confirm('Are you sure about this?')
+     if (reply) 
+          task.remove()
+}
 
 const addTask = (e) => {
      const task = input.value
      const li = document.createElement('li')
      const btn = document.createElement('button')
      
-     // Create list with a remove button
-     btn.innerHTML = '<i class="fas fa-trash-alt"></i>'
-     btn.addEventListener('click', () => li.remove())
-     li.innerHTML = task
+     if (task.trim() !== '') {
+               // Create list with a remove button
+               btn.innerHTML = '<i class="fas fa-trash-alt"></i>'
+               btn.addEventListener('click', () => deleteTask(li))
+               li.innerHTML = task
 
-     li.appendChild(btn)
-     todoList.appendChild(li)
-     input.value = ''
+               li.appendChild(btn)
+               todoList.appendChild(li)
+               input.value = ''
+          } else {
+               alert('You are missing a task!')
+          }
 }
 
 const clearList = () => todoList.innerHTML = ''
