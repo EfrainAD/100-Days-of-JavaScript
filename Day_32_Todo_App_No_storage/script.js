d69faead66c05c3a3e485cff65c228922cfee386
EfrainAD <105945226+EfrainAD@users.noreply.github.com>
2023-01-09 15:13:24 -0500

    updated to uses local storage

--- Day_32_Todo_App_No_storage/script.js
+++ Day_32_Todo_App_No_storage/script.js
@@ -3,32 +3,67 @@ const submit = document.querySelector('form button')
 const todoList = document.querySelector('.todo-list')
 const clear = document.querySelector('.clear')
 
-const deleteTask = (task) => {
+const deleteTask = (index) => {
      const reply = confirm('Are you sure about this?')
-     if (reply) 
-          task.remove()
+     if (reply) {
+          const tasks = getTasks()
+          tasks.splice(index, 1)
+          window.localStorage.setItem('tasks', JSON.stringify(tasks))
+          displayTasksList()
+     }
 }
-
-const addTask = (e) => {
-     const task = input.value
-     const li = document.createElement('li')
-     const btn = document.createElement('button')
+// Gets all tasks from Local server and turns it as an array
+const getTasks = () => {
+     const localStorage = window.localStorage.getItem('tasks')
      
-     if (task.trim() !== '') {
-               // Create list with a remove button
-               btn.innerHTML = '<i class="fas fa-trash-alt"></i>'
-               btn.addEventListener('click', () => deleteTask(li))
-               li.innerHTML = task
+     if (localStorage === null)
+         return []
+     else
+         return JSON.parse(localStorage)
+}
+// Update the list with tasks from the local storage
+const displayTasksList = () => {
+     const tasks = getTasks()
+     
+     todoList.innerHTML = ''
 
-               li.appendChild(btn)
-               todoList.appendChild(li)
-               input.value = ''
-          } else {
-               alert('You are missing a task!')
-          }
+     // Create list with a remove button
+     tasks.forEach((task, index) => {
+          const li = document.createElement('li')
+          const btn = document.createElement('button')
+
+          // Add delete button
+          btn.innerHTML = '<i class="fas fa-trash-alt"></i>'
+          btn.addEventListener('click', () => deleteTask(index))
+          li.innerHTML = task
+          li.appendChild(btn)
+          todoList.appendChild(li)
+     });
+}
+const addTask = () => {
+     newTask = input.value
+     let tasks = getTasks()
+     
+     // Validate
+     if (newTask.trim() === '') 
+          return alert('You are missing a task!')
+
+     // add task
+     tasks.push(newTask)
+     window.localStorage.setItem('tasks', JSON.stringify(tasks))
+     displayTasksList()
+     input.value = ''
 }
 
-const clearList = () => todoList.innerHTML = ''
+const clearList = () => {
+     const reply = confirm('You sure you want to delete everything?')
+     if (!reply) return
+     
+     window.localStorage.clear()
+     displayTasksList()
+}
 
 clear.addEventListener('click', clearList)
 submit.addEventListener('click', addTask)
+
+displayTasksList()
