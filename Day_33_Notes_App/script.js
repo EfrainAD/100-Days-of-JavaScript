ea31ace0bc6047ddfe544fbc33a6d8c5c5f30bd0
EfrainAD <105945226+EfrainAD@users.noreply.github.com>
2023-01-09 18:28:07 -0500

    No solution looked at, orginzed how I like it.

---
+++ Day_33_Notes_App/script.js
@@ -0,0 +1,94 @@
+// Add a Note Form
+const noteTitle = document.querySelector('#note-title')
+const noteText = document.querySelector('#note-text')
+const addNoteBtn = document.querySelector('#add-btn')
+// Display
+const clearAllBtn = document.querySelector('.clear')
+const notesContaner = document.querySelector('#notes')
+
+const getNotes = () => {
+     const notes = window.localStorage.getItem('notes')
+     if (notes === null) 
+          return []
+     else 
+          return JSON.parse(notes)
+}
+const getNote = (index) => {
+     const notes = getNotes()
+     return notes[index]
+}
+const displayNotes = () => {
+     const notes = getNotes()
+     
+     notesContaner.innerHTML = ''
+     
+     notes.forEach((note, index) => {
+          const {title, text} = note
+          const div = document.createElement('div')
+          
+          notesContaner.appendChild(div)
+          div.classList.add('note')
+          
+          div.innerHTML = `
+               <div class="note-cta">
+                    <p class="note-counter">Note ${index + 1}</p>
+                    <div class="note-cta-btn">
+                         <button class="note-btn" onclick="deleteNote(${index})">
+                              <i class="fas fa-trash"></i> Delete
+                         </button>
+                         <button class="note-btn edit-btn" onclick="updateNote(${index})">
+                              <i class="fas fa-edit"></i> Edit
+                         </button>
+                    </div>
+               </div>
+               <hr />
+               <h3 class="note-title">Title: ${title}</h3>
+               <p class="note-text">${text}</p>`
+     });
+     //  clear fields
+     noteTitle.value = ''
+     noteText.value = ''
+}
+const addNoteToLS = (note) => {
+     const notes = getNotes()
+     
+     notes.push(note)
+     window.localStorage.setItem('notes', JSON.stringify(notes))
+}
+const deleteNoteFromLS = (index) => {
+     const notes = getNotes()
+     notes.splice(index, 1)
+     window.localStorage.setItem('notes', JSON.stringify(notes))
+}
+const deleteNote = (index) => {
+     deleteNoteFromLS(index)
+     displayNotes()
+}
+const addNote = (e) => {
+     e.preventDefault()
+     const title = noteTitle.value
+     const text = noteText.value
+     const note = {title, text}
+
+     // Update Notes
+     addNoteToLS(note)
+     displayNotes()
+}
+const updateNote = (index) => {
+     const {title, text} = getNote(index) 
+     deleteNoteFromLS(index)
+     displayNotes()
+     noteTitle.value = title
+     noteText.value = text
+}
+const deleteAllNotes = () => {
+     const reply = confirm('Are you sure you want to delete everything?')
+     if (!reply) return 
+     window.localStorage.clear()
+     displayNotes()
+}
+
+addNoteBtn.addEventListener('click', addNote)
+clearAllBtn.addEventListener('click', deleteAllNotes)
+
+displayNotes()
