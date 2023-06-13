5c6c6469ccbbd916be31379b5746315f14a8684d
Efrain A. Davila <105945226+EfrainAD@users.noreply.github.com>
2023-06-13 11:14:15 -0400

    Day 38

--- Day_38_Temperature_Converter/script.js
+++ Day_38_Temperature_Converter/script.js
@@ -1,11 +1,38 @@
-// --Convert Celsius to--
-// fah = (x * 1.8) + 32;
-// kelvin = x + 273.15;
+const form = document.querySelector('.convertion-form')
+const inputFields = document.querySelectorAll('.convertion-form input')
 
-// -- Convert Fah to --
-// celsius = (x - 32) / 1.8;
-// kelvin = (x - 32) / 1.8 + 273.15;
+const conversions = {
+   celsius: {
+      fahrenheit: (C) => C * 1.8 + 32,
+      kelvin: (C) => C + 273.15,
+   },
+   fahrenheit: {
+      celsius: (F) => (F - 32) / 1.8,
+      kelvin: (F) => (F - 32) / 1.8 + 273.15,
+   },
+   kelvin: {
+      celsius: (K) => K - 273.15,
+      fahrenheit: (K) => (K - 273.15) * 1.8 + 32,
+   },
+}
 
-// -- Conver Kelvin to --
-// fah = (x - 273.15) * 1.8 + 32;
-// celsius = parseFloat(x) - 273.15;
+// helper function
+const clearField = ({ target }) => (target.value = '')
+
+// main function
+const convert = (e) => {
+   const temp = parseFloat(e.target.value)
+   const convertFrom = e.target.placeholder.toLowerCase()
+
+   inputFields.forEach((inputField) => {
+      const convertTo = inputField.placeholder.toLowerCase()
+
+      if (conversions[convertFrom] && conversions[convertFrom][convertTo]) {
+         inputField.value = conversions[convertFrom][convertTo](temp).toFixed(3)
+      }
+   })
+}
+
+// Add Event Listeners
+form.addEventListener('input', convert)
+inputFields.forEach((input) => input.addEventListener('focus', clearField))
