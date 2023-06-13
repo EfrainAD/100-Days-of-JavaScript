ca4c86d5e3ccfbc9bb89775fc9fcb71aaf53d095
Efrain A. Davila <105945226+EfrainAD@users.noreply.github.com>
2023-06-13 09:40:47 -0400

    Day 38 setup

---
+++ Day_38_Temperature_Converter/script.js
@@ -0,0 +1,11 @@
+// --Convert Celsius to--
+// fah = (x * 1.8) + 32;
+// kelvin = x + 273.15;
+
+// -- Convert Fah to --
+// celsius = (x - 32) / 1.8;
+// kelvin = (x - 32) / 1.8 + 273.15;
+
+// -- Conver Kelvin to --
+// fah = (x - 273.15) * 1.8 + 32;
+// celsius = parseFloat(x) - 273.15;
