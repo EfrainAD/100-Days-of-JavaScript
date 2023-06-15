228ba78d9526242e410fa28eebc543b27513f7bf
Efrain A. Davila <105945226+EfrainAD@users.noreply.github.com>
2023-06-15 17:28:03 -0400

    Day 40 1. Refractered 2. some formatting update to make it look nice

--- Day_40_Word_Count_Tool/script.js
+++ Day_40_Word_Count_Tool/script.js
@@ -1,4 +1,6 @@
-const WORD_LIMIT = 500
+const WORD_LIMIT = 300
+const READING_SPEED_WPM = 250
+const READING_SPEED_WPS = READING_SPEED_WPM / 60
 
 const inputText = document.querySelector('.input')
 const character = document.querySelector('.character')
@@ -15,7 +17,39 @@ const getWordCount = (str) => {
 }
 const getCharacterCount = (str) => str.length
 const caluWordsRemaining = (wordCount) => WORD_LIMIT - wordCount
-const getReadingTime = (wordCount) => wordCount / 50
+
+// Helper Function - For Formating
+const getFormatedSeconds = (totalSeconds) => {
+   const s = totalSeconds % 60
+
+   if (!s) return ''
+   if (totalSeconds < 3) {
+      const secondsRounded = Math.round(s * 100) / 100
+      return `${secondsRounded}s`
+   }
+   return `${Math.floor(s)}s`
+}
+const getFormatedMinutes = (totalSeconds) => {
+   const m = Math.floor((totalSeconds % 3600) / 60)
+
+   if (!m) return ''
+   else return `${m}m `
+}
+const getFormatedHours = (timeInSeconds) => {
+   const h = Math.floor(timeInSeconds / 3600)
+   if (!h) return ''
+   else return `${h}h `
+}
+
+const getReadingTime = (wordCount) => {
+   const timeInSeconds = wordCount / READING_SPEED_WPS
+
+   const s = getFormatedSeconds(timeInSeconds)
+   const m = getFormatedMinutes(timeInSeconds)
+   const h = getFormatedHours(timeInSeconds)
+
+   return `${h}${m}${s}`
+}
 
 // Main Function
 const updateCounts = () => {
